626b8895997cb42037f6f07d92ce0881f76b786b
BrandonEJenkins <brandonejenkins@gmail.com>
2020-04-27 12:31:39 -0500

    added functions to generate random characters prevent default sync the spanner with the text field and scripted DOM elements to grab checkboxes from html doc

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,104 @@
 
 //DOM getElement
-const passwordEl = document.getElementById('password');
-const uppercaseEl = document.getElementById('includeUppercase');
-const lowercaseEl = document.getElementById('includeLowercase');
-const numbersEl = document.getElementById('includeNumbers');
-const symbolsEl = document.getElementById('includeSymbols');
+const passwordLengthRange = document.getElementById('passwordLengthRange');
+const passwordLengthNumber = document.getElementById('passwordLengthNumber');
 
+const includeUppercaseEl = document.getElementById('includeUppercase');
+const includeLowercaseEl = document.getElementById('includeLowercase');
+const includeNumbersEl = document.getElementById('includeNumbers');
+const includeSymbolsEl = document.getElementById('includeSymbols');
+
+const form = document.getElementById('passwordGeneratorForm')
+
+const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
+  arrayFromLowtoHigh(58, 64)
+).concat(
+  arrayFromLowtoHigh(91, 96)
+).concat(
+  arrayFromLowtoHigh(123, 126)
+)
+
+// const generateEl = document.getElementById('generate');
+// const passwordEl = document.getElementById('password');
+
+// Set up event listeners
+passwordLengthNumber.addEventListener('input', syncPasswordAmount)
+passwordLengthRange.addEventListener('input', syncPasswordAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const passwordLength = passwordLengthNumber.value
+  const uppercaseEl = includeUppercaseEl.checked
+  const lowercaseEl = includeLowercaseEl.checked
+  const numbersEl = includeNumbersEl.checked
+  const symbolsEl = includeSymbolsEl.checked
+
+  const finalPassword = generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSymbols)
+})
+
+function boxesChecked () {
+  if ( includeLowercase.checked || includeUppercase.checked || includeNumbers.checked || includeSymbols.checked ) {
+    document.querySelector(":disabled").disabled = false;
+  }
+}
+
+function generatePassword(passwordLength, uppercaseEl, lowercaseEl, numbersEl, symbolsEl) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (uppercaseEl) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (symbolsEl)charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  //String.fromCharCode(65)
+
+  const passwordCharacters = []
+  for (let i = 0; i < passwordLength, i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * passwordLength)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+
+function arrayFromLowtoHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function syncPasswordAmount(e) {
+  const value = e.target.value
+  passwordLengthNumber.value = value
+  passwordLengthRange.value = value
+}
 
 // Store functions in one object
-const groupFunc = {
-  upper: genRandomUpper,
-  lower: genRandomLower,
-  number: genRandomNumber,
-  symbol: genRandomSymbol
-};
+// const groupFunc = {
+//   upper: genRandomUpper,
+//   lower: genRandomLower,
+//   number: genRandomNumber,
+//   symbol: genRandomSymbol
+// };
 
 
 //Functions to generate random characters - Reference: codepen.io - Reference: net.comber.com/charset
-function genRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// function genRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
 
-function genRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function genRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-function genRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+// function genRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
 
-function genRandomSymbol() {
-  const symbols = '!@#$%^&*(){}[]=<>,.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
+// function genRandomSymbol() {
+//   const symbols = '!@#$%^&*(){}[]=<>,.';
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
 
 
 
@@ -92,7 +158,7 @@ var confirmSymbols = confirm('Include special characters?');
 // const generateEl = document.getElementById('generate');
 
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// 
 
 
 
