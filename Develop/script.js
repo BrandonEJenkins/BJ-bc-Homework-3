8c7397db36fa2bcff3aa85d748759b5ebb2cc7fc
BrandonEJenkins <brandonejenkins@gmail.com>
2020-04-27 22:38:59 -0500

    modified css styles to add 3d feel to form container password field and button also finalized js script

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,16 @@
 
-//DOM getElement
-const passwordLengthRange = document.getElementById('passwordLengthRange');
-const passwordLengthNumber = document.getElementById('passwordLengthNumber');
+// DOM elements
+const characterLengthRange = document.getElementById('characterAmountRange');
+const characterLengthNumber = document.getElementById('characterAmountNumber');
 
 const includeUppercaseEl = document.getElementById('includeUppercase');
-const includeLowercaseEl = document.getElementById('includeLowercase');
 const includeNumbersEl = document.getElementById('includeNumbers');
 const includeSymbolsEl = document.getElementById('includeSymbols');
 
-const form = document.getElementById('passwordGeneratorForm')
+const form = document.getElementById('passwordGeneratorForm');
+const passwordDisplay = document.getElementById('passwordDisplay');
 
+// Generate arrays using browser character sets to define password criteria
 const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
@@ -21,39 +22,32 @@ const SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
   arrayFromLowtoHigh(123, 126)
 )
 
-// const generateEl = document.getElementById('generate');
-// const passwordEl = document.getElementById('password');
+// Set up event listeners and sync slider to number input form
+characterLengthNumber.addEventListener('input', syncCharacterAmount)
+characterLengthRange.addEventListener('input', syncCharacterAmount)
 
-// Set up event listeners
-passwordLengthNumber.addEventListener('input', syncPasswordAmount)
-passwordLengthRange.addEventListener('input', syncPasswordAmount)
 
+// Set up submit event listener to call variables if checkboxes are checked
 form.addEventListener('submit', e => {
   e.preventDefault()
-  const passwordLength = passwordLengthNumber.value
-  const uppercaseEl = includeUppercaseEl.checked
-  const lowercaseEl = includeLowercaseEl.checked
-  const numbersEl = includeNumbersEl.checked
-  const symbolsEl = includeSymbolsEl.checked
-
-  const finalPassword = generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSymbols)
+  const characterLength = characterLengthNumber.value
+  const hasUppercase = includeUppercaseEl.checked
+  const hasNumbers = includeNumbersEl.checked
+  const hasSymbols = includeSymbolsEl.checked
+  const password = generatePassword(characterLength, hasUppercase, hasNumbers, hasSymbols)
+  passwordDisplay.innerText = password
 })
 
-function boxesChecked () {
-  if ( includeLowercase.checked || includeUppercase.checked || includeNumbers.checked || includeSymbols.checked ) {
-    document.querySelector(":disabled").disabled = false;
-  }
-}
-
-function generatePassword(passwordLength, uppercaseEl, lowercaseEl, numbersEl, symbolsEl) {
+// Create function to generate random characters and concatenate into string using for loop until password equals total password length
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
-  if (uppercaseEl) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (symbolsEl)charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-  //String.fromCharCode(65)
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
 
   const passwordCharacters = []
-  for (let i = 0; i < passwordLength, i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * passwordLength)]
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join('')
@@ -67,114 +61,8 @@ function arrayFromLowtoHigh(low, high) {
   return array
 }
 
-function syncPasswordAmount(e) {
+function syncCharacterAmount(e) {
   const value = e.target.value
-  passwordLengthNumber.value = value
-  passwordLengthRange.value = value
+  characterLengthNumber.value = value
+  characterLengthRange.value = value
 }
-
-// Store functions in one object
-// const groupFunc = {
-//   upper: genRandomUpper,
-//   lower: genRandomLower,
-//   number: genRandomNumber,
-//   symbol: genRandomSymbol
-// };
-
-
-//Functions to generate random characters - Reference: codepen.io - Reference: net.comber.com/charset
-// function genRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// function genRandomLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function genRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-
-// function genRandomSymbol() {
-//   const symbols = '!@#$%^&*(){}[]=<>,.';
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-
-
-
-
-
-/************************************
-* GRAVEYARD
-
-// do while loop; length must be a number greater than 8 less than 128
-/*
-
-do {
-  var passLength = parseInt(window.prompt('Please enter the password length (must be a NUMBER from 8 to 128)',''), 10);
-} while ((isNaN(passLength) || passLength > 128 || passLength < 8) || (isNaN(passLength) || typeof passLength !== "number"));
-
-do {
-var confirmUppers = confirm('Include uppercase letters?');
-var confirmLowers = confirm('Include lowercase letters?');
-var confirmNumbers = confirm('Include numbers?');
-var confirmSymbols = confirm('Include special characters?');
-
-  if (confirmUppers === false && confirmLowers === false && confirmNumbers === false && confirmSymbols === false) {
-  alert('Must choose at least 1 character type');
-  }
-} while (confirmUppers === false && confirmLowers === false && confirmNumbers === false && confirmSymbols === false); {
-  
-  // document.write('Password includes uppercase: ' + confirmUppers),
-  // document.write('Password includes lowercase: ' + confirmLowers),
-  // document.write('Password includes uppercase: ' + confirmNumbers),
-  // document.write('Password includes uppercase: ' + confirmSymbols);
-}
-
-*/
-
-// function generate() {
-//   var password = '';
-//   for (let i = 1; index <= 10; i++) {
-//     var c = Math.floor((Math.random() * x.length) + 0);
-//     password += x.charAt(c);
-//   }
-//   console.log(password);
-//   document.getElementById('pass').innerHTML = `<h1>Password: ${password}</h1>`;
-// }
-
-
-// function generate() {
-//   var password='';
-//   for (let i = 1; i < 128 && i > 8; i++) {
-//     var c = Math.floor((Math.random() * x.length) + 0);
-//     password += x.chartAt(c);
-//   }
-// }
-
-
-// DOM elements
-// const passwordEl = document.getElementById('password');
-// const generateEl = document.getElementById('generate');
-
-// Assignment Code
-// 
-
-
-
-
-// Generate password click event
-// generateEl.addEventListener('click', () => {
-//   const length = 
-// })
-
-//Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
